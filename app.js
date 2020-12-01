26bfef991ba8bb201442c604f45046e0da247604
Xavier Burrow <xavierb@temphost.local>
2020-12-02 10:18:13 +1100

    data

--- app.js
+++ app.js
@@ -11,22 +11,58 @@ class App {
     };
     this.el.run.onclick = () => {
       const t = performance.now();
-      this.day1();
+      this.day1_HASH();
       this.el.time.value = `${performance.now() - t} ms`;
     };
   }
 
+  day1_HASH() {
+    const data = this.el.input.value.trim().split('\n').map(e => parseInt(e));
+    const iLim = data.length-1;
+    const jLim = data.length;
+    const len = data.length;
+    let p1 = null;
+    let p2 = null;
+    let iter = 0;
+    const map1 = {};
+    const map2 = {};
+
+    for (let i=0; i<len; i++) {
+      iter++;
+      const v = data[i];
+      const d = 2020 - v;
+
+      if (d < 0)
+        continue;
+
+      if (map1[v]) {
+        p1 = map1[v].val * v;
+
+        console.log(v, map1[v], p1);
+      } else {
+        map1[d] = {
+          val: v,
+        };
+      }
+    }
+
+    console.log('Iterations: ' + iter);
+  }
+
   day1() {
-    const input = this.el.input.value;
-    const data = input.split('\n').map(e => parseInt(e));
+    const data = this.el.input.value.trim().split('\n').map(e => parseInt(e));
     const iLim = data.length-1;
     const jLim = data.length;
     let p1 = null;
     let p2 = null;
+    let iter = 0;
 
+    // loop
     br:
     for (let i=0; i<iLim; i++) {
+      iter++;
       for (let j=i+1; j<jLim; j++) {
+        iter++;
         const sum2 = data[i] + data[j];
         if (sum2 > 2020)
           continue;
@@ -38,6 +74,7 @@ class App {
         if (p2)
           continue;
         for (let k=0; k<jLim; k++) {
+          iter++;
           if (k == i || k == j || sum2 + data[k] !== 2020)
             continue;
           p2 = data[i] * data[j] * data[k];
@@ -49,7 +86,7 @@ class App {
 
     console.log(p1);
     console.log(p2);
-    this.el.output.value = p1 + '\n' + p2;
+    this.el.output.value = 'p1: ' + p1 + '\np2: ' + p2 + '\niterations: ' + iter;
   }
 }
 
