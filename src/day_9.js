7978d2f7ffdd831e6c94cf55b5ce3e3ff0c31545
meatbags <jxburrow@gmail.com>
2020-12-10 18:15:34 +1100

    stash

--- src/day_9.js
+++ src/day_9.js
@@ -2,54 +2,50 @@
 
 class Day8 {
   solve(text) {
-    // utils
-    const removeNewline = s => s.replace(/[\n\r]/g, '');
-    const nl = '\n';
-
     // parse
-    let lines = text.trim().split(nl).map(n => parseInt(n));
-    console.log('Length:', lines.length);
-    console.log(lines);
+    let list = text.trim().split('\r\n').map(n => parseInt(n));
 
-    const sum2 = (list, target) => {
-      for (let i=0; i<list.length-1; i++) {
-        for (let j=1; j<list.length; j++) {
-          if (list[i] + list[j] == target) {
-            return true;
-          }
-        }
-      }
-      return false;
-    }
+    // part 1
+    let p1 = null;
+    let i = 24;
+    while (!p1) {
+      let target = list[++i];
+      let map = {};
 
-    let p1 = 0;
-    for (let i=25; i<lines.length; i++) {
-      const target = lines[i];
-      const arr = lines.slice(i-25, i);
-      if (!this.sum2(arr, target)) {
-        p1 = target;
-        break;
-      }
-    }
-    console.log('P1:', p1);
-
-    // p2
-    for (let i=0; i<lines.length; i++) {
-      let acc = lines[i];
-      for (let j=i+1; j<lines.length; j++) {
-        acc += lines[j];
-        if (acc == p1) {
-          let arr = lines.slice(i, j+1);
-          const min = Math.min(...arr);
-          const max = Math.max(...arr);
-          console.log(min, max, min + max);
+      for (let j=i-25; j<i; j++) {
+        let x = list[j];
+        if (map[target - x])
+          break;
+        if (j == i-1) {
+          p1 = target;
           break;
         }
-        if (acc > p1) {
-          break;
-        }
+        map[x] = true;
       }
     }
+    console.log(p1);
+
+    // part 2
+    let p2 = null;
+    let lower = 0;
+    let upper = 0;
+    let acc = 0;
+
+    loop:
+    while (true) {
+      while (acc < p1)
+        acc += list[upper++];
+
+      while (acc > p1)
+        acc -= list[lower++];
+
+      if (acc == p1) {
+        const arr = list.slice(lower, upper);
+        p2 = Math.min(...arr) + Math.max(...arr);
+        break loop;
+      }
+    }
+    console.log(p2);
   }
 }
 
